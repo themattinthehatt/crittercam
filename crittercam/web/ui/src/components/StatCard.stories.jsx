71ff8640592221a924ffb347af10a433c754dc37
Matt Whiteway <themattinthehatt@gmail.com>
2026-05-23 14:44:48 -0400

    statcard component

---
+++ crittercam/web/ui/src/components/StatCard.stories.jsx
@@ -0,0 +1,41 @@
+import StatCard from './StatCard'
+
+export default {
+  title: 'Primitives/StatCard',
+  component: StatCard,
+}
+
+export const Images = {
+  args: {
+    value: 142,
+    label: 'images',
+  },
+}
+
+export const Detections = {
+  args: {
+    value: 37,
+    label: 'detections',
+  },
+}
+
+export const Species = {
+  args: {
+    value: 8,
+    label: 'species',
+  },
+}
+
+export const Zero = {
+  args: {
+    value: 0,
+    label: 'detections',
+  },
+}
+
+export const LargeNumber = {
+  args: {
+    value: 10432,
+    label: 'images',
+  },
+}
